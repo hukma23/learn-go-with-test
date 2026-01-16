5aa9f71ae39dfbd7a892d66fae3b78de52a15be1
hukma <muhammadhukmaulilalbab@gmail.com>
2026-01-16 22:18:38 +0700

    belajar hello world pada go with test

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,8 +3,36 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "English")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when empty string is supplied", func(t *testing.T) {
+		got := Hello("", "English")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Hukma", "French")
+		want := "Bonjour, Hukma"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
