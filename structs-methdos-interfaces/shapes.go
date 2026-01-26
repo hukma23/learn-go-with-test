24c7b59d42f6ca1d80d81e7563001d4657e01908
hukma <muhammadhukmaulilalbab@gmail.com>
2026-01-26 19:17:18 +0700

    belajar structs, interfaces, methods

---
+++ structs-methdos-interfaces/shapes.go
@@ -0,0 +1,35 @@
+package structsmethdosinterfaces
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+type Circle struct {
+	Radius float64
+}
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func Perimeter(shape Rectangle) float64 {
+	return 2 * (shape.Width + shape.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
--- structs-methdos-interfaces/shapes_test.go
+++ structs-methdos-interfaces/shapes_test.go
@@ -3,10 +3,29 @@ package structsmethdosinterfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTest := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTest {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+		}
 	}
 }
