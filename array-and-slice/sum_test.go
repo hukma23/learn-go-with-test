17ce3c4dd009d4bdaa1a114b701712d31125288f
hukma <muhammadhukmaulilalbab@gmail.com>
2026-01-26 13:52:09 +0700

    structs, methods, interface

--- array-and-slice/sum_test.go
+++ array-and-slice/sum_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -30,7 +34,27 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
-	if got != want {
+	if !slices.Equal(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of some slice", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
---
+++ structs-methdos-interfaces/go.mod
@@ -0,0 +1,3 @@
+module example.com/structs-methods-interfaces
+
+go 1.25.6
